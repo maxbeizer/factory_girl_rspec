7eca68a95a83159c199165ca953435b67dce2d37
max beizer <max.beizer@gmail.com>
2016-09-02 17:52:31 -0500

    Add find_definitions hack

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,4 +2,8 @@ require 'factory_girl'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:all) do
+    FactoryGirl.find_definitions if FactoryGirl.factories.count == 0
+  end
 end
