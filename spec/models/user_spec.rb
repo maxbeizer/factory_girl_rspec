2f544ec5cd5ecb20074a94cbcfc2094b28b895e2
max beizer <max.beizer@gmail.com>
2016-09-02 19:30:31 -0500

    Do not test the presence that is no longer validated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of :email }
-    it { should validate_presence_of :admin }
 
     it "ensures email is unique" do
       user_one = create(:user, email: 'unique@example.com')
